03b118702fda7e5f813f30c6d29c57e1bb035ba0
FlorianBodart <bodart.florian@outlook.fr>
2022-04-30 10:02:17 +0200

    version 0.0.2 create gravity

--- index.js
+++ index.js
@@ -56,12 +56,27 @@ const enemy = new Sprite({
 
 console.log(player);
 
+const keys = {
+    q: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    if (keys.q.pressed) {
+        player.velocity.x = -1
+    } else if (keys.d.pressed) {
+        player.velocity.x = 1
+    }
 }
 
 animate()
@@ -69,7 +84,10 @@ animate()
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 1
+            keys.d.pressed = true
+            break
+        case 'q':
+            keys.q.pressed = true
             break
     }
     console.log(event.key)
@@ -78,7 +96,10 @@ window.addEventListener('keydown', (event) => {
 window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 0
+            keys.d.pressed = false
+            break
+        case 'q':
+            keys.q.pressed = false
             break
     }
     console.log(event.key)
