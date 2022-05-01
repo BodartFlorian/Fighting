d036ca5a38abdceaa2838800754e561b435ad2f0
FlorianBodart <bodart.florian@outlook.fr>
2022-05-01 16:59:16 +0200

    version 0.0.8 fixed bug  timer

--- index.js
+++ index.js
@@ -126,12 +126,28 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     )
 }
 
-let timer = 10
+function determineWinner({player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie'
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Win'
+    } else if (player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Win'
+    }
+}
+
+let timer = 60
+let timerId
 function decreaseTimer() {
-    setTimeout(decreaseTimer, 1000)
     if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
         timer--
         document.querySelector('#timer').innerHTML = timer
+    }
+    if (timer === 0) {
+        determineWinner({player, enemy, timerId})
     }
 }
 
@@ -184,6 +200,11 @@ function animate() {
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({player, enemy, timerId})
     }
 }
 
