961a2ca92d5b3e90d3309eef17abf28cad445649
FlorianBodart <bodart.florian@outlook.fr>
2022-05-04 12:27:16 +0200

    version 0.1.2 add enemy animation

--- js/classes.js
+++ js/classes.js
@@ -110,10 +110,12 @@ class Fighter extends Sprite {
         // gravity function
         if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
             this.velocity.y = 0
+            this.position.y = 330
         } else this.velocity.y += gravity
     }
 
     attack() {
+        this.switchSprite('attack1')
         this.isAttacking = true
         setTimeout(() => {
             this.isAttacking = false
@@ -121,6 +123,7 @@ class Fighter extends Sprite {
     }
     
     switchSprite(sprite) {
+        if (this.image === this.sprites.attack1.image && this.framesCurrent < this.sprites.attack1.framesMax -1 ) return
         switch (sprite) {
             case 'idle':
                 if (this.image !== this.sprites.idle.image) {
@@ -147,6 +150,13 @@ class Fighter extends Sprite {
                 if (this.image !== this.sprites.fall.image) {
                 this.image = this.sprites.fall.image
                 this.framesMax = this.sprites.fall.framesMax
+                this.framesCurrent = 0
+                }
+                break
+            case 'attack1':
+                if (this.image !== this.sprites.attack1.image) {
+                this.image = this.sprites.attack1.image
+                this.framesMax = this.sprites.attack1.framesMax
                 this.framesCurrent = 0
                 }
                 break
